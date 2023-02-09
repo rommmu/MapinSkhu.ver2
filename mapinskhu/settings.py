729cc58b9874a3f40174d3c83b3b984f0d11d564
김수진 <ksj030120@gmail.com>
2023-02-09 23:57:55 +0900

    Modify DB

--- mapinskhu/settings.py
+++ mapinskhu/settings.py
@@ -132,12 +132,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'classApp', 'static')
+    os.path.join(BASE_DIR, 'static'),
 ]
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
