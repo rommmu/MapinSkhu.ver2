39a157a96929d162b5251c2312c762a5610802af
GraceKim527 <lemonherb0323@naver.com>
2023-02-19 19:45:22 +0900

    revert: 관, 메인, introduce 복구 완료

--- classApp/models.py
+++ classApp/models.py
@@ -4,17 +4,17 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Classes(models.Model):
-    kind = models.CharField(max_length=50)
-    code = models.CharField(max_length=50)
-    class_name = models.CharField(max_length=300)
-    prof = models.CharField(max_length=100)
+    kind = models.CharField(max_length=50) # 교필/전선/전필 등
+    code = models.CharField(max_length=50) # 과목코드
+    class_name = models.CharField(max_length=300) # 강의명
+    prof = models.CharField(max_length=100) # 교수명
 
-    room = models.CharField(max_length=100, null=True)
-    date1 = models.CharField(max_length=5, null=True)
-    date2 = models.CharField(max_length=5, null=True)
-    start = models.TimeField(max_length=10, null=True)
-    end = models.TimeField(max_length=10, null=True)
-    kwan_name = models.CharField(max_length=50, null=True)
+    room = models.CharField(max_length=100, null=True) # 강의실
+    date1 = models.CharField(max_length=5, null=True) # 첫째요일
+    date2 = models.CharField(max_length=5, null=True) # 둘째요일
+    start = models.TimeField(max_length=10, null=True) # 시작시간
+    end = models.TimeField(max_length=10, null=True) # 끝시간
+    kwan_name = models.CharField(max_length=50, null=True) # 강의관명
 
     def __str__(self):
         return f'{self.kind, self.code, self.class_name, self.prof, self.room, self.date1, self.date2, self.start, self.end, self.kwan_name}'
