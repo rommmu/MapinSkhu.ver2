4705821bc8e53379c5b55bbcb9b691cd728e04f4
김수진 <ksj030120@gmail.com>
2023-03-06 05:08:58 +0900

    fe 수정 중

--- static/js/index.js
+++ static/js/index.js
@@ -549,10 +549,3 @@ function search_hide() {
 }
 
 // 고친부분
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-window.addEventListener('resize', () => {
-   let vh = window.innerHeight * 0.01;
-   document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
