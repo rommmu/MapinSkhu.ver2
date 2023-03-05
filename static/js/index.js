1d23f8fadafe4c4f334ea3940a459987f5b1b3da
김수진 <ksj030120@gmail.com>
2023-03-06 04:47:49 +0900

    fe 수정 중

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,4 @@
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-window.addEventListener('resize', () => {
-   let vh = window.innerHeight * 0.01;
-   document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
 
 
 // 각 건물 이벤트
